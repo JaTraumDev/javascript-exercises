5fd7ce835027760a63efa0a9be6919a72fa0393f
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-12 19:02:56 +0100

    Finish 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverseText = "";
+    for(let i = text.length; i > 0; i--){
+        reverseText += text[i - 1];
+    }
+
+    return reverseText;
 
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;

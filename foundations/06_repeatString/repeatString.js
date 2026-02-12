05ac4ff0f41a2767b66a1a77bacc7c34009d9761
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-12 18:47:05 +0100

    Finish 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    arr = "";
+    if(times < 0){
+        return "ERROR";
+    } else {
+        for(let i = 0; i < times; i++){
+            arr += text;
+        }
+        return arr;
+    }
+    
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;

1de363d41ba09c5d2d070a553727564d897af1a9
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-13 18:22:38 +0100

    Finish 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0 &&Number.isInteger(year)){
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+console.log(leapYears(1200));
 
 // Do not edit below this line
 module.exports = leapYears;

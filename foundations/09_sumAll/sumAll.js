f5854e81f418f49724ceb278aaf0f5ade7f25059
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-13 18:16:08 +0100

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start = 0, end = 0) {
+    
+    let arr = [];
+
+    if(start < 0 || end < 0 || start % 1 != 0 || end % 1 !== 0 || typeof start === "string" || typeof end === "string"){
+        return "ERROR";
+    }
+
+    if(start > end){
+        for(let i = end; i <= start; i++){
+            arr.push(i);
+        }
+    } else {
+        for(let i = start; i <= end; i++){
+            arr.push(i);
+        }
+    }
+    
+    return arr.reduce((total, items) => {
+        return total + items;
+    }, 0)
 
 };
+
+sumAll(10, );
 
 // Do not edit below this line
 module.exports = sumAll;

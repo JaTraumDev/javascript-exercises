3e634b49761e8406e0b098175095f8abd3c4118d
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-23 17:56:55 +0100

    Finish 15_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function sortByOldestPerson() {
+    const currentYear = new Date().getFullYear();
 
+    return (a, b) => {
+        const ageA = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+
+        return ageB - ageA;
+    };
+}
+const findTheOldest = function (people) {
+    people.sort(sortByOldestPerson());
+    return people[0];
 };
 
 // Do not edit below this line

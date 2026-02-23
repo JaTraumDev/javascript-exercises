bcd176a4dccd01f2bee9f0858721648f443f8593
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-23 16:32:30 +0100

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text = "") {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanedText = text
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphabet.includes(character))
+        .join();
+
+    reversedText = cleanedText.split("").reverse().join();
+
+    return reversedText === cleanedText;
 };
+
+console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
